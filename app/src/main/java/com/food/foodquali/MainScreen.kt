d0e7a6ea31df9ddd2d2727b4e5195bc26b52bfa3
lanzy <gerlan.masterpay@gmail.com>
2024-09-15 11:21:17 +0800

    changes mades

--- app/src/main/java/com/food/foodquali/MainScreen.kt
+++ app/src/main/java/com/food/foodquali/MainScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
